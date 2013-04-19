b9aa51658b410bac1d4a9cc7c5232c6cbd4732f7
Tristan Lins <tristan.lins@bit3.de>
2013-04-19 21:47:52 +0200

    Initial commit.

---
+++ bin/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+# switch to composer directory
+cd ../composer || exit 1
+
+# cleanup
+rm -r cache vendor composer.lock composer.phar
+
+# install composer.phar
+curl -sS https://getcomposer.org/installer | php || exit 1
+
+# install dependencies
+php composer.phar install || exit 1
+
+# change directory up
+cd ../ || exit 1
+
+# pack archive
+zip --symlinks -r dist/contao-composer.zip composer/composer.lock composer/vendor system/modules || exit 1
